8d8bae245c4ed4590c05a8794230f013644212c0
Alex Louzada <alexlouzada2009@gmail.com>
2019-10-06 02:15:51 -0300

    Busca de parametros pronta

--- Js/main.js
+++ Js/main.js
@@ -2,9 +2,26 @@
 
 // const camera = document.getElementById('camera')
 let slideNumber = 0;
+let params = getParams();
 
 function moveTo(x, y, z) {
     camera.object3D.position.set(x, y, z);
+}
+
+function getParams() {
+    var pairs = window.location.search.substring(1).split("&"),
+      obj = {},
+      pair,
+      i;
+  
+    for ( i in pairs ) {
+      if ( pairs[i] === "" ) continue;
+  
+      pair = pairs[i].split("=");
+      obj[ decodeURIComponent( pair[0] ) ] = decodeURIComponent( pair[1] );
+    }
+  
+    return obj;
 }
 
 // computerImage.addEventListener('mouseenter', () => {
@@ -33,9 +50,10 @@ function moveTo(x, y, z) {
 
 function changeSlide(diference) {
     if ((diference < 0 && slideNumber >=1) || (diference > 0 && slideNumber < 6)) {
+        if(!params.materia) params.materia = "Slide";
         slideNumber += diference;
         document.getElementById("computerImage").setAttribute("color", "");
-        document.getElementById("slideImage").setAttribute("src", "Assets/Imagens/Slide"+slideNumber+".PNG");
-        document.getElementById("computerImage").setAttribute("src", "Assets/Imagens/Slide"+slideNumber+".PNG");
+        document.getElementById("computerImage").setAttribute("src", "Assets/Imagens/"+params.materia+slideNumber+".PNG");
+        document.getElementById("slideImage").setAttribute("src", "Assets/Imagens/"+params.materia+slideNumber+".PNG");
     }
 }
