cf403203c0a5d2ee1877ed5a31473cb0a8fc0055
Alex Louzada <alexlouzada2009@gmail.com>
2019-10-05 22:21:24 -0300

    Sala de aula finalizada

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,40 @@
-const camera = document.getElementById('camera')
+// import { cursorTo } from "readline";
+
+// const camera = document.getElementById('camera')
+let slideNumber = 0;
 
 function moveTo(x, y, z) {
-    console.warn(x+','+y+','+z);
     camera.object3D.position.set(x, y, z);
+}
+
+// computerImage.addEventListener('mouseenter', () => {
+//     if(isPlaying) {
+//         cursor.setAttribute('visible', false);
+//     }
+// })
+
+// computerImage.addEventListener('mouseleave', () => {
+//     if(isPlaying) {
+//         cursor.setAttribute('visible', true);
+//     }
+// })
+
+// slideImage.addEventListener('mouseenter', () => {
+//     if(isPlaying) {
+//         cursor.setAttribute('visible', false);
+//     }
+// })
+
+// slideImage.addEventListener('mouseleave', () => {
+//     if(isPlaying) {
+//         cursor.setAttribute('visible', true);
+//     }
+// })
+
+function changeSlide(diference) {
+    if ((diference < 0 && slideNumber >=1) || (diference > 0 && slideNumber < 6)) {
+        slideNumber += diference;
+        document.getElementById("slideImage").setAttribute("src", "Assets/Imagens/Slide"+slideNumber+".PNG");
+        document.getElementById("computerImage").setAttribute("src", "Assets/Imagens/Slide"+slideNumber+".PNG");
+    }
 }
